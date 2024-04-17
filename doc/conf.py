ea09f1890064473a7ba9675ed637d6c87445d622
alaindef <alain.de.feyter@gmail.com>
2024-04-17 21:34:55 +0200

    version 0.4.3.2 a

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ author = 'Karel Tavernier'
 
 # The full version, including alpha/beta/rc tags
 # adf 240417
-# release = '0.4.3.1'
+# release = '0.4.3.2'
 
 
 # -- General configuration ---------------------------------------------------
